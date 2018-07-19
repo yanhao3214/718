4b3da1a7650ec23fc4dfe3da77c5968ca18475ef
yanhao3214 <yanhaonuaa@163.com>
2018-07-19 11:02:48 +0800

    添加core模块，配置BaseActivity和BaseFragment

---
+++ core/src/main/java/com/yh/core/app/BaseActivity.java
@@ -0,0 +1,98 @@
+package com.yh.core.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+
+/**
+ * @author: 闫昊
+ * @date: 2018/7/19
+ * @function:
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initWindow();
+
+        if (initArgs(getIntent().getExtras())) {
+            int layoutId = setContent();
+            setContentView(layoutId);
+            initView();
+            initData();
+        } else {
+            finish();
+        }
+    }
+
+    /**
+     * 初始化窗口
+     */
+    protected void initWindow() {
+    }
+
+    /**
+     * 处理传给当前Activity的参数
+     *
+     * @param bundle 参数bundle
+     * @return 参数正确返回true，参数错误返回false
+     */
+    protected boolean initArgs(Bundle bundle) {
+        return true;
+    }
+
+    /**
+     * 设置布局资源
+     *
+     * @return 布局ID
+     */
+    protected abstract int setContent();
+    // TODO: 2018/7/19  重载View
+
+    /**
+     * 初始化控件
+     */
+    protected void initView() {
+    }
+
+    /**
+     * 初始化数据
+     */
+    protected void initData() {
+    }
+
+    /**
+     * 点击界面导航返回时，finish当前页面
+     *
+     * @return
+     */
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+    }
+
+    /**
+     * 先返回最上层Fragment，再销毁Activity
+     */
+    @Override
+    public void onBackPressed() {
+        List<Fragment> fragments = getSupportFragmentManager().getFragments();
+        if (fragments.size() > 0) {
+            for (Fragment fragment : fragments) {
+                if (fragment instanceof BaseFragment) {
+                    //若拦截返回事件则直接return
+                    if (((BaseFragment) fragment).onBackPressed()) {
+                        return;
+                    }
+                }
+            }
+        }
+        super.onBackPressed();
+        finish();
+    }
+}
---
+++ core/src/main/java/com/yh/core/app/BaseFragment.java
@@ -0,0 +1,89 @@
+package com.yh.core.app;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author: 闫昊
+ * @date: 2018/7/19
+ * @function:
+ */
+public abstract class BaseFragment extends Fragment {
+    protected View mRootView;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        initArgs(getArguments());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (mRootView == null) {
+            int layoutId = setContent();
+            mRootView = inflater.inflate(layoutId, container, false);
+            initView(mRootView);
+        } else if (mRootView.getParent() != null) {
+            ((ViewGroup) mRootView.getParent()).removeView(mRootView);
+        }
+        return mRootView;
+    }
+
+    /**
+     * 布局加载完成后初始化数据、findViewById，避免页面切换卡顿
+     *
+     * @param view
+     * @param savedInstanceState
+     */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initData();
+    }
+
+    /**
+     * 设置布局资源
+     *
+     * @return 布局ID
+     */
+    protected abstract int setContent();
+
+    /**
+     * 初始化数据
+     */
+    protected void initData() {
+    }
+
+    /**
+     * 初始化控件
+     *
+     * @param rootView
+     */
+    protected void initView(View rootView) {
+
+    }
+
+    /**
+     * 处理传给当前Fragment的参数
+     *
+     * @param bundle
+     */
+    protected void initArgs(Bundle bundle) {
+    }
+
+    /**
+     *
+     * @return true表示拦截返回逻辑，fragment返回上层、Activity不销毁
+     *          false表示不拦截返回逻辑，Activity自行处理返回逻辑（销毁）
+     */
+    protected boolean onBackPressed() {
+        return false;
+    }
+}
